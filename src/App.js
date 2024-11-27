62615650ef5a7e7ce32e6117ac7bb5eb689d30ea
Yurosiezhang <rosiezzzyyy@gmail.com>
2024-11-28 00:42:38 +1030

    Add FeedbackItem component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+
 import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
+
 
 
 function App() {
@@ -7,7 +10,7 @@ function App() {
         <>
         <Header />
         <div className="container">
-            <h1>Feedbacks</h1>
+            < FeedbackItem/>
         </div>
         </>
     )
