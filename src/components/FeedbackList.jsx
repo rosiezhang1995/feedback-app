828875b13284167f1c6207fb6b2300cb2489f5f5
Yurosiezhang <rosiezzzyyy@gmail.com>
2024-11-28 15:11:48 +1030

    Add Card component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
-import FeedbackItem from './FeedbackItem'
 
+import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
 function FeedbackList({ feedback }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
@@ -12,6 +13,17 @@ function FeedbackList({ feedback }) {
       ))}
     </div>
   )
+}
+
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
 }
 
 export default FeedbackList;
