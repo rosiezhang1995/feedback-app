3bcedada255a214fa75e2c980471821b21977f5a
Yurosiezhang <rosiezzzyyy@gmail.com>
2024-12-01 23:41:19 +1030

    Add Feeback item fade animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-
+import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 function FeedbackList({ feedback, handleDelete}) {
@@ -8,13 +8,22 @@ function FeedbackList({ feedback, handleDelete}) {
 
   return (
     <div className='feedback-list'>
+      <AnimatePresence>
       {feedback.map((item) => (
+        <motion.div
+        key={item.id}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        >
         <FeedbackItem 
         key={item.id} 
         item={item} 
         handleDelete={handleDelete}
         />
+        </motion.div>
       ))}
+      </AnimatePresence>
     </div>
   )
 }
