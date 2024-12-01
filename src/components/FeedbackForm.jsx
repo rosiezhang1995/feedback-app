3e4d84d98da630e391249afa569bcfcb3c547208
Yurosiezhang <rosiezzzyyy@gmail.com>
2024-12-01 23:22:13 +1030

    Implement feedback submission and assign unique ID to each entry

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from './shared/Card'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
 
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
@@ -36,15 +36,32 @@ function FeedbackForm() {
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10){
+            const newFeedback = {
+                text,
+                rating
+            }
+            
+            handleAdd(newFeedback)
+
+            setText('') // Reset text
+        }
+    }
+
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
                 <RatingSelect select={(rating) => setRating(rating)} />
                 <div className='input-group'>
                     <input
                         onChange={handleTextChange}
-                        type="text" placeholder='Write a review...' />
+                        type="text" 
+                        placeholder='Write a review...' 
+                        value={text}                      
+                        />
                     <Button type='submit' isDisabled={btnDisabled}>
                         Send
                     </Button>
@@ -52,7 +69,6 @@ function FeedbackForm() {
                 {/* Display validation message (if any) */}
                 {message && <div className='message'>{message}</div>}
             </form>
-
         </Card>
 
     )
