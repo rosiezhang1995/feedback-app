09a3c3d5ec9b01cd328b1a65bdd84bf5319ac319
Yurosiezhang <rosiezzzyyy@gmail.com>
2024-12-11 19:40:58 +1030

    Create Context and Provider and format code.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,59 @@
-import { v4 as uuidv4 } from 'uuid'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useState } from "react"
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
-import FeedbackStats from "./components/FeedbackStats"
-import FeedbackForm from "./components/FeedbackForm"
-import AboutPage from './pages/AboutPage'
-import AboutIconLink from './components/AboutIconLink'
-
-
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-    const [feedback, setFeedback] = useState(FeedbackData)
-    
-    // Add new feedback with assigning id.
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
-      }
+  // Add new feedback with assigning id.
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')){
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-    
+  };
 
-    return (
-        <Router>
-            <Header />
-            <div className="container">
-                <Routes>
-                    <Route exact path='/' element={
-                        <>
-                            <FeedbackForm handleAdd={addFeedback}/>
-                            <FeedbackStats feedback={feedback}/>
-                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-                        </>
-                    } />
-                    
-                    <Route path='/about' element={<AboutPage />} />
-                </Routes>
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            />
 
-                <AboutIconLink />
-            </div>
-        </Router>
-    )
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  );
 }
 
 export default App;
