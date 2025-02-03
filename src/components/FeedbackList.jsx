19ae7c8390aa2182a69aa82b415767ddbc229a46
Yurosiezhang <rosiezzzyyy@gmail.com>
2025-02-03 21:09:22 +1030

    Fetch data from backend json-server and add loading spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,36 +1,36 @@
-import { motion, AnimatePresence } from 'framer-motion'
-import FeedbackItem from './FeedbackItem'
-import PropTypes from 'prop-types'
-import { useContext } from 'react'
-import FeedbackContext from '../context/FeedbackContext'
+import { motion, AnimatePresence } from 'framer-motion';
+import FeedbackItem from './FeedbackItem';
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import Spinner from './shared/Spinner';
+import FeedbackContext from '../context/FeedbackContext';
+
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext)
-  
-  if (!feedback || feedback.length === 0) {
-    return <p>No Feedback Yet</p>
+  const { feedback, isLoading } = useContext(FeedbackContext);
+
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>;
   }
 
-  return (
-    <div className='feedback-list'>
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className="feedback-list">
       <AnimatePresence>
-      {feedback.map((item) => (
-        <motion.div
-        key={item.id}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        >
-        <FeedbackItem 
-        key={item.id} 
-        item={item} 
-        />
-        </motion.div>
-      ))}
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
       </AnimatePresence>
     </div>
-  )
+  );
 }
-
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
@@ -40,6 +40,6 @@ FeedbackList.propTypes = {
       rating: PropTypes.number.isRequired,
     })
   ),
-}
+};
 
 export default FeedbackList;
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,30 @@
-import { v4 as uuidv4 } from "uuid";
-import { createContext, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { createContext, useState, useEffect } from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 7,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
-  })
+  });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch Feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:3001/feedback?_sort=id&_order=desc`
+    );
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   // Add new feedback with assigning id.
   const addFeedback = (newFeedback) => {
@@ -35,7 +34,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Delete feedbcak
   const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
+    if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
@@ -44,22 +43,23 @@ export const FeedbackProvider = ({ children }) => {
   const updateFeedback = (id, updItem) => {
     setFeedback(
       feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
-    )
-  }
+    );
+  };
 
   // Set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
       item,
-      edit: true
-    })
-  }
+      edit: true,
+    });
+  };
 
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
